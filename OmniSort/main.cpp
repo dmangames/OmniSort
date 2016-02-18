370f2ebf433929fbb335fc7482fb85a97d72e33d
Steven Zhang <stevenzh2012@gmail.com>
2016-02-18 10:50:45 -0600

    Initial Commit

---
+++ OmniSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OmniSort
+//
+//  Created by Steven Zhang on 2/18/16.
+//  Copyright © 2016 Dmangames. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
